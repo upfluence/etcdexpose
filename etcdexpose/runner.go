54d07636eebc69007c79445744cb51b3801da4f1
Julien Levesy <jlevesy@gmail.com>
2015-07-06 21:04:37 -0700

    implemented runner

--- etcdexpose/runner.go
+++ etcdexpose/runner.go
@@ -2,17 +2,41 @@ package etcdexpose
 
 import (
 	"github.com/coreos/go-etcd/etcd"
+	"log"
 )
 
 type Handler interface {
-	Handle(*etcd.Response)
+	Perform(*etcd.Response)
+}
+
+type DumbHandler struct {
+}
+
+func (d *DumbHandler) Perform(e *etcd.Response) {
+	log.Printf("HI THERE")
 }
 
 type Runner struct {
 	Watcher *EtcdWatcher
-	Handler *Handler
+	Handler Handler
 }
 
-func NewRunner(watcher *EtcdWatcher, handler *Handler) (*Runner, error) {
-	return &Runner{Watcher: nil, Handler: nil}, nil
+func NewRunner(watcher *EtcdWatcher, handler Handler) *Runner {
+	return &Runner{
+		Watcher: watcher,
+		Handler: handler,
+	}
+}
+
+func (r *Runner) Start() {
+	go r.Watcher.Start()
+	for {
+		select {
+		case event := <-r.Watcher.EventChan:
+			log.Printf("Received a new event %s", event.Action)
+			r.Handler.Perform(event)
+		case err := <-r.Watcher.ErrorChan:
+			log.Fatal("Error %s", err)
+		}
+	}
 }
