fb40276eb94b6e24e6d32b743511114e13920640
Julien Levesy <jlevesy@gmail.com>
2015-07-12 17:22:13 -0700

    defined etcdclient

---
+++ etcdexpose/etcdclient.go
@@ -0,0 +1,36 @@
+package etcdexpose
+
+import (
+	"github.com/coreos/go-etcd/etcd"
+	"log"
+)
+
+type EtcdClient struct {
+	client    *etcd.Client
+	namespace string
+	key       string
+}
+
+func NewEtcdClient(
+	client *etcd.Client,
+	namespace string,
+	key string,
+) *EtcdClient {
+	return &EtcdClient{
+		client:    client,
+		namespace: namespace,
+		key:       key,
+	}
+}
+
+func (e *EtcdClient) ReadNamespace() (*etcd.Response, error) {
+	return e.client.Get(e.namespace, false, false)
+}
+
+func (e *EtcdClient) WriteValue(value string) (*etcd.Response, error) {
+	resp, err := e.client.Set(e.key, value, 0)
+	if err == nil {
+		log.Printf("Updated %s to %s", e.key, value)
+	}
+	return resp, err
+}
