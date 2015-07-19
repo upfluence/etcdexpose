9d58432df0fcd2a0e93cfc951aa5357a070f0ca2
Julien Levesy <jlevesy@gmail.com>
2015-07-18 21:24:49 -0700

    use a buffered channel for stop sync

--- etcdexpose/etcdwatcher.go
+++ etcdexpose/etcdwatcher.go
@@ -9,22 +9,19 @@ type EtcdWatcher struct {
 	Namespace string
 	client    *etcd.Client
 	stopChan  chan bool
-	running   bool
 }
 
 func NewEtcdWatcher(namespace string, cli *etcd.Client) *EtcdWatcher {
 	return &EtcdWatcher{
 		Namespace: namespace,
 		client:    cli,
-		running:   false,
 	}
 }
 
 func (e *EtcdWatcher) Start(eventChan chan *etcd.Response, errorChan chan error) {
 	log.Printf("Begining to watch key %s", e.Namespace)
 
-	e.stopChan = make(chan bool)
-	e.running = true
+	e.stopChan = make(chan bool, 1)
 
 	_, err := e.client.Watch(
 		e.Namespace,
@@ -32,13 +29,10 @@ func (e *EtcdWatcher) Start(eventChan chan *etcd.Response, errorChan chan error)
 		true,
 		eventChan,
 		e.stopChan)
-	e.running = false
 	errorChan <- err
 	return
 }
 
 func (e *EtcdWatcher) Stop() {
-	if e.running {
-		e.stopChan <- true
-	}
+	e.stopChan <- true
 }
